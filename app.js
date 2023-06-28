946b05690d38cf42e6d193ca9c4e6640aa2762cf
Yogesh Madar <77493546+yogeshmadar@users.noreply.github.com>
2023-06-28 10:43:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -45,18 +45,18 @@ app.options('*', cors());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Set security HTTP headers
-app.use(helmet());
+//app.use(helmet());
 
 app.use(
-helmet.contentSecurityPolicy({
-directives: {
-defaultSrc: ['self'", 'https:', 'http:', 'data:', 'ws:'], 
-baseUri: ['self'"], 
-styleSrc: ['self'", "'unsafe-inline"", 'https:', 'http:'],
-fontSrc: ['self'", 'https:', 'http:', 'data:'],
-scriptsrc: ['self'", 'https:', 'http:', 'blob:'],
-},
-})
+ helmet.contentSecurityPolicy({
+  directives: {
+    defaultSrc: ['self'", 'https:', 'http:', 'data:', 'ws:'], 
+    baseUri: ['self'"], 
+    styleSrc: ['self'", "'unsafe-inline"", 'https:', 'http:'],
+    fontSrc: ['self'", 'https:', 'http:', 'data:'],
+    scriptsrc: ['self'", 'https:', 'http:', 'blob:'],
+  },
+ })
 );
 
 // Development logging
